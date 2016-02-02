37a7bbe4cfd3ac02b3ce980e44ab2b3be913119c
Maxime Brazeilles <maxime.brazeilles@gmail.com>
2016-02-02 17:22:20 +0100

    outlook background-color

--- src/components/Button.js
+++ src/components/Button.js
@@ -36,7 +36,7 @@ class Button extends Component {
 
     return _.merge({}, this.constructor.baseStyles, {
       td: {
-        backgroundColor: mjAttribute('background-color'),
+        background: mjAttribute('background-color'),
         borderRadius: mjAttribute('border-radius'),
         color: mjAttribute('color'),
         fontStyle: mjAttribute('font-style'),
@@ -48,7 +48,7 @@ class Button extends Component {
         borderRadius: mjAttribute('border-radius')
       },
       a: {
-        backgroundColor: mjAttribute('background-color'),
+        background: mjAttribute('background-color'),
         border: `1px solid ${mjAttribute('background-color')}`,
         borderRadius: mjAttribute('border-radius'),
         color: mjAttribute('color'),

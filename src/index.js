4aebbc884ab9ad9de24492fd3c590761ea55f5ff
Loëck Vézien <nainpuissant@gmail.com>
2016-02-29 18:14:02 +0100

    Byebye require().default

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ export documentParser from './documentParser'
 export mjml2html from './mjml2html'
 export MJMLColumnElement from './components/decorators/MJMLColumnElement'
 export MJMLElement from './components/decorators/MJMLElement'
-export const elements = require('./MJMLElementsCollection').default
-export const registerElement = require('./MJMLElementsCollection').registerElement
+export elements from './MJMLElementsCollection'
+export { registerElement } from './MJMLElementsCollection'
 export const version = () => process.env.MJML_VERSION

b82f55f9527ff90ab1c2cd481f6f86cc51c1a5f7
Loëck Vézien <nainpuissant@gmail.com>
2016-03-08 13:42:47 +0100

    Fix tagName on mj-raw when parent is mj-column

--- src/components/Raw.js
+++ src/components/Raw.js
@@ -6,14 +6,29 @@ import React, { Component } from 'react'
 })
 class Raw extends Component {
 
+  getTagName () {
+    const { parentMjml } = this.props
+    let tagName
+
+    switch (parentMjml.get('tagName')) {
+      case 'mj-column' :
+        tagName = 'tr'
+        break
+
+      default:
+        tagName = 'noscript'
+    }
+
+    return tagName
+  }
+
   render () {
     const { mjContent } = this.props
 
-    return (
-      <tr
-        className="mj-raw"
-        dangerouslySetInnerHTML={{ __html: mjContent() }} />
-    )
+    return React.createElement(this.getTagName(), {
+      className: 'mj-raw',
+      dangerouslySetInnerHTML: { __html: mjContent() }
+    })
   }
 
 }

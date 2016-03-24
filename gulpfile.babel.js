74395abb0238e57ac3edacc07ca8f5ec9fb9ad7b
Loëck Vézien <nainpuissant@gmail.com>
2016-03-24 12:39:10 +0100

    Clean, add gulpfile

---
+++ gulpfile.babel.js
@@ -0,0 +1,104 @@
+import { argv } from 'yargs'
+import { cd, exec, rm } from 'shelljs'
+import fs from 'fs'
+import gulp from 'gulp'
+import path from 'path'
+
+const ROOT_PATH = path.resolve(__dirname)
+const PACKAGES_PATH = path.resolve(__dirname, './packages')
+const packages = fs.readdirSync(PACKAGES_PATH).filter(file => {
+  return fs.statSync(path.resolve(PACKAGES_PATH, file)).isDirectory()
+}).reduce((acc, file) => ({
+  ...acc,
+  [file]: path.resolve(PACKAGES_PATH, file)
+}), {})
+
+const sharedDeps = [
+  'react'
+]
+gulp.task('install', () => {
+  return Promise.all(
+    // Link all packages to the root
+    Object.keys(packages).map(packageName => {
+      return new Promise(resolve => {
+        cd(packages[packageName])
+        exec('npm link')
+        cd(ROOT_PATH)
+        exec(`npm link ${packageName}`)
+        resolve()
+      })
+    })
+  ).then(() => {
+    // Remove duplicated packages and shared dependencies so they are loaded
+    // from the top
+    return Promise.all(
+      Object.keys(packages).map(packageName => {
+        return Promise.all(
+          Object.keys(packages).concat(sharedDeps).map(dependencyName => {
+            return new Promise(resolve => {
+              rm('-rf', path.resolve(packages[packageName], 'node_modules', dependencyName))
+              resolve()
+            })
+          })
+        )
+      })
+    )
+  })
+})
+
+gulp.task('clean', () => {
+  // Remove package node_modules
+  return Promise.all(
+    Object.keys(packages).map(packageName => {
+      return new Promise(resolve => {
+        rm('-rf', path.resolve(packages[packageName], 'node_modules'), path.resolve(packages[packageName], 'lib'), path.resolve(packages[packageName], 'dist'))
+        resolve()
+      })
+    })
+  )
+})
+
+gulp.task('version', () => {
+  // Try to derive package name from directory where this was run from
+  const pwd = process.env.PWD
+  const pwdPackageName = Object.keys(packages).reduce((prev, name) => {
+    return packages[name] === pwd ? name : prev
+  }, undefined)
+
+  // Check params
+  const packageName = argv.pkg || argv.p || pwdPackageName
+  const version = argv.version || argv.v
+  if (!packageName || !version) {
+    throw new Error('Usage: gulp version -p <package> -v <version>')
+  }
+
+  // Bump the version
+  cd(packages[packageName])
+  const execResult = exec(`npm version ${version}`)
+  const bumpedVersion = execResult.output.replace('\n', '').replace('v', '')
+
+  // Commit and tag
+  exec(`git add ${packages[packageName]}/package.json`)
+  const message = `${packageName}@${bumpedVersion}`
+  exec(`git commit -m "${message}"`)
+  const tagName = `${packageName}-v${bumpedVersion}`
+  exec(`git tag ${tagName}`)
+})
+
+gulp.task('publish', () => {
+  // Try to derive package name from directory where this was run from
+  const pwd = process.env.PWD
+  const pwdPackageName = Object.keys(packages).reduce((prev, name) => {
+    return packages[name] === pwd ? name : prev
+  }, undefined)
+
+  // Check params
+  const packageName = argv.pkg || argv.p || pwdPackageName
+  if (!packageName) {
+    throw new Error('Usage: gulp publish -p <package>')
+  }
+
+  // Publish
+  cd(packages[packageName])
+  exec('npm publish')
+})

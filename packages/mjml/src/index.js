574683aa6b3ca80b207595f8c83ff38398f19c10
Maxime Brazeilles <maxime.brazeilles@gmail.com>
2016-03-31 02:45:17 +0200

    Binary wrapping, still not "babelify" well...

--- packages/mjml/src/index.js
+++ packages/mjml/src/index.js
@@ -1,1 +1,34 @@
+import { registerElement } from 'mjml-core'
+import Body from 'mjml-body'
+import Button from 'mjml-button'
+import Column from 'mjml-column'
+import Divider from 'mjml-divider'
+import Html from 'mjml-html'
+import Image from 'mjml-image'
+import InvoiceItem from 'mjml-invoice-item'
+import Invoice from 'mjml-invoice'
+import List from 'mjml-list'
+import Location from 'mjml-location'
+import Raw from 'mjml-raw'
+import Section from 'mjml-section'
+import Social from 'mjml-social'
+import Table from 'mjml-table'
+import Text from 'mjml-text'
+
+[ Body,
+  Button,
+  Column,
+  Divider,
+  Html,
+  Image,
+  InvoiceItem,
+  Invoice,
+  List,
+  Location,
+  Raw,
+  Section,
+  Social,
+  Table,
+  Text ].map((element) => registerElement(element))
+
 export * from 'mjml-core'

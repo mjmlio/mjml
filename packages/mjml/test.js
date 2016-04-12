79f91a5693eb33514a0f92400593b8b0f1ff5ac3
swibge <matthieu.pinte@gmail.com>
2016-04-12 18:52:53 +0200

    testing

--- packages/mjml/test.js
+++ packages/mjml/test.js
@@ -5,14 +5,27 @@ console.log(
   <mjml>
     <content>
       <mj-body>
-        <mj-navbar base-url="http://google.com/" background-color="red">
-          <mj-navbar-column width="70%" text-align="center">
+        <mj-navbar base-url="http://google.com/" background-color="#f0f0f0">
+          <mj-navbar-column width="70%" text-align="left">
             <mj-navbar-link href="#1" color="#42adea">Un lien</mj-navbar-link>
             <mj-navbar-link href="#2" color="#42adea">Deux liens</mj-navbar-link>
             <mj-navbar-link href="#3" color="#42adea">Trois liens</mj-navbar-link>
-            <mj-navbar-link href="#4" color="#42adea">Quatre liens</mj-navbar-link>
-            <mj-navbar-link href="#5" color="#42adea">Cinq liens</mj-navbar-link>
           </mj-navbar-column>
+          <mj-navbar-column width="30%">
+            <mj-navbar-link href="#4" color="#42adea">Quatre liens</mj-navbar-link>
+            <mj-navbar-link href="#5" color="#42adea">(+)</mj-navbar-link>
+          </mj-navbar>
+        </mj-navbar>
+
+        <mj-navbar base-url="http://google.com/" background-color="#f0f0f0">
+          <mj-navbar-column width="70%" text-align="left">
+            <mj-navbar-link href="#1" color="#42adea">Un lien</mj-navbar-link>
+            <mj-navbar-link href="#2" color="#42adea">Deux liens</mj-navbar-link>
+            <mj-navbar-link href="#3" color="#42adea">Trois liens</mj-navbar-link>
+          </mj-navbar-column>
+          <mj-navbar-column width="30%">
+            <mj-button>Un button</mj-button>
+          </mj-navbar>
         </mj-navbar>
       </mj-body>
     </content>

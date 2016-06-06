f25ba87cbad01f20ded894b6c67713e453ae6214
Guillaume Badi <gbadi@student.42.fr>
2016-06-06 16:30:02 +0200

    mjml extensions

--- packages/mjml-core/src/includeExternal.js
+++ packages/mjml-core/src/includeExternal.js
@@ -9,7 +9,10 @@ const getContent = input =>
     .replace(/<\/mj-container>[\n\s\t]+<\/mj-body>[\n\s\t]+<\/mjml>/, '')
 
 export default mjml => mjml.replace(includes, (_, path) => {
-  const template = fs.readFileSync(path, 'utf8')
+
+  const mjmlExtension = file => file.trim().match(/.mjml$/) && file || `${file}.mjml`
+
+  const template = fs.readFileSync(mjmlExtension(path), 'utf8')
   const content = getContent(template)
 
   if (!content) { throw new Error(`Error while parsing file: ${path}`) }

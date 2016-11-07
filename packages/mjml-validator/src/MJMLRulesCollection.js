518c9c010cd37dcbfe84d79d0e314b8419052eb8
Julien Fournier <fournier.j@sfr.fr>
2016-11-07 14:44:47 +0100

    Return on warning

--- packages/mjml-validator/src/MJMLRulesCollection.js
+++ packages/mjml-validator/src/MJMLRulesCollection.js
@@ -3,13 +3,17 @@ import * as rules from './rules'
 
 const MJMLRulesCollection = rules
 
-export const registerMJRule = rule => {
+export const registerMJRule = (rule, name) => {
 
   if (typeof rule != 'function' || rule.length !== 1) {
-    warning(false, 'Your rule must be a function and must have one parameter which is the element to validate')
+    return warning(false, 'Your rule must be a function and must have one parameter which is the element to validate')
   }
 
-  MJMLRulesCollection[rule.name] = rule;
+  if (name) {
+    MJMLRulesCollection[name] = rule;
+  } else {
+    MJMLRulesCollection[rule.name] = rule;
+  }
 
 }
 

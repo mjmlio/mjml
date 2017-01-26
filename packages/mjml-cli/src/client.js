221b28a23778574fb66f34072819ea90fb47d036
Adam Yi <xuan@yiad.am>
2017-01-27 03:52:56 +0800

    Use stdout to output if stdin is used for input and no output file is specified

--- packages/mjml-cli/src/client.js
+++ packages/mjml-cli/src/client.js
@@ -51,7 +51,7 @@ const render = (bufferPromise, { min, output, stdout, fileName, level }) => {
         handleError(availableErrorOutputFormat['text'](errors))
       }
 
-      if (stdout) {
+      if (stdout || (!output)) {
         process.stdout.write(html)
       } else {
         return write(output, html)

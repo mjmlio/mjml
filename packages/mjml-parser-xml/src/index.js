4958503985683cc44e639d42cbdf7add2a148d1d
Maxime Brazeilles <maxime.brazeilles@gmail.com>
2017-06-01 16:39:51 +0200

    fix endingtags

--- packages/mjml-parser-xml/src/index.js
+++ packages/mjml-parser-xml/src/index.js
@@ -26,8 +26,7 @@ export default function parseXML (xml, options = {}) {
   const {
     addEmptyAttributes = true,
     CDATASections = _.chain({
-      ...components.head,
-      ...components.body,
+      ...components
     })
       .filter(component => component.prototype.endingTag)
       .map(component => component.getName())

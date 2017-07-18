6720414c9cdeeb51afab8bc3131d309b068b7a6f
Maxime Brazeilles <maxime.brazeilles@gmail.com>
2017-07-18 14:55:09 +0200

    Add both method to fix yahoo + ie overflow
    https://www.emailonacid.com/forum/viewthread/824/

--- packages/mjml-core/src/configs/defaultContainer.js
+++ packages/mjml-core/src/configs/defaultContainer.js
@@ -18,6 +18,16 @@ export default () => {
     @viewport { width:320px; }
   }
 </style>
+<style>
+@media screen yahoo {
+  * {
+  overflow: visible!important
+  }
+  .y-overflow-hidden {
+  overflow: hidden!important
+  }
+}
+</style>
 <!--<![endif]-->
 <!--[if mso]>
 <xml>

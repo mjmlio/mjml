5cb4d51dacd7a494b82c5ec947b0d3420966ef31
Vladimir Zdenek <auchEnia@users.noreply.github.com>
2017-09-20 16:55:52 +0200

    feature: mj-style - enable external css
    Use external CSS in mj-style by providing an "src" attribute.

--- packages/mjml-head-style/src/index.js
+++ packages/mjml-head-style/src/index.js
@@ -1,7 +1,12 @@
+const fs = require('fs')
+
 export default {
   name: "mj-style",
   endingTag: true,
   handler: ({ content, attributes }, globalAttributes) => {
+    if (attributes.src) {
+      content = fs.readFileSync(attributes.src, 'utf8')
+    }
     if ( attributes.inline == "inline") {
       globalAttributes.inlineCSS.push(content)
     } else {

bebf2a036ec8d6c80b7179623a7f61c1b2d2ea9f
cavrois cedric <cavrois.cedric@hotmail.fr>
2018-01-18 15:12:10 +0100

    handle includes in validation error message

--- packages/mjml-validator/src/rules/ruleError.js
+++ packages/mjml-validator/src/rules/ruleError.js
@@ -1,10 +1,21 @@
 export default function ruleError(message, element) {
-  const { line, tagName } = element
+  const { line, tagName, includedIn } = element
 
   return {
     line,
     message,
     tagName,
-    formattedMessage: `Line ${line} (${tagName}) — ${message}`,
+    formattedMessage: `Line ${line}${formatInclude(element)} (${tagName}) — ${message}`,
   }
+}
+
+function formatInclude(element) {
+  const { includedIn, file: parentFile } = element
+  if (!includedIn.length) return ''
+
+  const formattedIncluded = includedIn.slice().reverse().map(({line, file}) => {
+    return `line ${line} of file ${file}`
+  }).join(', itself included at ')
+
+  return ` of ${parentFile}, included at ${formattedIncluded}`
 }

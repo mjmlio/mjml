05049e11a24e84002105b3bbcd31f3601bbaab85
KMcB <cavrois.cedric@hotmail.fr>
2018-04-27 18:38:25 +0200

    dont include padding in width if set width

--- packages/mjml-image/src/index.js
+++ packages/mjml-image/src/index.js
@@ -83,7 +83,7 @@ export default class MjImage extends BodyComponent {
 
     const contentWidth = min([
       parseInt(containerWidth, 10) - allPaddings,
-      width + paddingLeft + paddingRight
+      width
     ])
 
     return `${contentWidth}px`

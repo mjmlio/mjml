5748f4548b65afb9def2b019417152d65882ba2f
KMcB <cavrois.cedric@hotmail.fr>
2018-06-18 15:41:50 +0200

    refacto into helper

--- packages/mjml-parser-xml/src/index.js
+++ packages/mjml-parser-xml/src/index.js
@@ -24,6 +24,11 @@ const indexesForNewLine = xml => {
   return indexes
 }
 
+const isSelfClosing = (indexes, parser) => {
+  return indexes.startIndex === parser.startIndex &&
+         indexes.endIndex === parser.endIndex
+}
+
 export default function MJMLParser(xml, options = {}, includedIn = []) {
   const {
     addEmptyAttributes = true,
@@ -187,16 +192,12 @@ export default function MJMLParser(xml, options = {}, includedIn = []) {
         cur = newNode
       },
       onclosetag: name => {
-
         if (endingTags.indexOf(name) !== -1) {
           inEndingTag -= 1
 
           if (!inEndingTag) { // we're getting out of endingTag
-            // if not self-closing tags (in which case we don't get the content)
-            if (
-              currentEndingTagIndexes.startIndex !== parser.startIndex &&
-              currentEndingTagIndexes.endIndex !== parser.endIndex
-            ) {
+            // if self-closing tag we don't get the content
+            if (!isSelfClosing(currentEndingTagIndexes, parser)) {
               const val = xml.substring(currentEndingTagIndexes.endIndex + 1, parser.startIndex).trim()
               if (val) cur.content = val
             }

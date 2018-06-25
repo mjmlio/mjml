821d1b3eaea9f1f9b04799db3fc3534c6fcf5b71
KMcB <cavrois.cedric@hotmail.fr>
2018-06-25 22:06:09 +0200

    fix for head components

--- packages/mjml-core/src/helpers/formatAttributes.js
+++ packages/mjml-core/src/helpers/formatAttributes.js
@@ -2,7 +2,7 @@ import { reduce } from 'lodash'
 import { initializeType } from '../types/type'
 
 export default (attributes, allowedAttributes) => reduce(attributes, (acc, val, attrName) => {
-  if (allowedAttributes[attrName]) {
+  if (allowedAttributes && allowedAttributes[attrName]) {
     const TypeConstructor = initializeType(allowedAttributes[attrName])
 
     if (TypeConstructor) {

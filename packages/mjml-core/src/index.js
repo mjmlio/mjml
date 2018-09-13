9e8685101b007a280efef21b6b1cce8a03d70f61
KMcB <cavrois.cedric@hotmail.fr>
2018-09-13 16:44:52 +0200

    fix cli --validate option

--- packages/mjml-core/src/index.js
+++ packages/mjml-core/src/index.js
@@ -295,6 +295,6 @@ export default function mjml2html(mjml, options = {}) {
 
 handleMjmlConfig(process.cwd(), registerComponent)
 
-export { components, initComponent, registerComponent, suffixCssClasses, handleMjmlConfig }
+export { components, initComponent, registerComponent, suffixCssClasses, handleMjmlConfig, initializeType }
 
 export { BodyComponent, HeadComponent } from './createComponent'

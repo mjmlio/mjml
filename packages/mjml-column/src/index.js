1a06fced1f8a09e9d08be2822e35195e85f05b6e
Mantas Miliukas <mantasm@wix.com>
2018-10-05 16:36:42 +0300

    fix(mj-column): change background to bgcolor

--- packages/mjml-column/src/index.js
+++ packages/mjml-column/src/index.js
@@ -192,7 +192,7 @@ export default class MjColumn extends BodyComponent {
     return `
       <table
         ${this.htmlAttributes({
-          background: this.getAttribute('background-color'),
+          bgcolor: this.getAttribute('background-color'),
           border: '0',
           cellpadding: '0',
           cellspacing: '0',

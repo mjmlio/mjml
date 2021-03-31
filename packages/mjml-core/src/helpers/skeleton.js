d469886b2a3889c1c210cf912fe48920f1a8611a
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2021-03-31 12:29:19 +0200

    fix invalid HTML comments in skeleton

--- packages/mjml-core/src/helpers/skeleton.js
+++ packages/mjml-core/src/helpers/skeleton.js
@@ -29,9 +29,9 @@ export default function skeleton(options) {
         <title>
           ${title}
         </title>
-        <!--[if !mso]><!-->
+        <!--[if !mso]>
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <!--<![endif]-->
+        <![endif]-->
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <style type="text/css">

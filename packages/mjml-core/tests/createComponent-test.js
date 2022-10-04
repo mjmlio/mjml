ec1e7e11cb049fa7fa9980e9b43ef0d514a680b5
Asher Cohen <asherlc@asherlc.com>
2022-10-04 10:45:47 -0700

    fix test

--- packages/mjml-core/tests/createComponent-test.js
+++ packages/mjml-core/tests/createComponent-test.js
@@ -1,38 +1,33 @@
 /* eslint-disable max-classes-per-file */
 const chai = require('chai')
-const { Component } = require('../src/createComponent')
+const { BodyComponent } = require('../lib/createComponent')
 
-
-class MockParent extends Component {
+class MockParent extends BodyComponent {
   tagName = 'mock-parent'
 }
 
-class MockChild extends Component {
+class MockChild extends BodyComponent {
   tagName = 'mock-child'
 }
 
-
-  const child = new MockChild(
-    {
-      tagName: 'mj-text',
-      attributes: {
-        'font-size': '20px',
-        color: '#F45E43',
-        'font-family': 'helvetica',
-      },
-      content: 'Hello World',
-    },
-  )
-
-  
+const child = new MockChild({
+  attributes: {
+    'font-size': '20px',
+    color: '#F45E43',
+    'font-family': 'helvetica',
+  },
+  content: 'Hello World',
+})
 
 const parent = new MockParent({
-    tagName: 'mj-column',
-    children: [child],
-    attributes: {},
+  children: [child],
+  attributes: {},
 })
 
 const mjml = parent.renderAsMJML()
 
-
-chai.expect(mjml, 'renderAsMJML test failed').to.equal('')
+chai
+  .expect(mjml, 'renderAsMJML test failed')
+  .to.equal(
+    '<mock-parent><mock-child font-size="20px" color="#F45E43" font-family="helvetica"></mock-child></mock-parent>',
+  )

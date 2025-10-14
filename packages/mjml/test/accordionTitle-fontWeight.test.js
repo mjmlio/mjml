ad769e8c2d802ce7aa7870ec9c25b7a0df467c9c
Daz Greer <darren.greer@sinch.com>
2025-10-14 15:49:54 +0200

    chore(testing): Update local testing MJML-46
    Added Mocha to dev dependencies and a 'test' script to run local automated tests. Updated existing tests to use it.

--- packages/mjml/test/accordionTitle-fontWeight.test.js
+++ packages/mjml/test/accordionTitle-fontWeight.test.js
@@ -2,57 +2,61 @@ const chai = require('chai')
 const { load } = require('cheerio')
 const mjml = require('../lib')
 
-const input = `
-<mjml>
-  <mj-head>
-    <mj-attributes>
-      <mj-accordion border="none" padding="1px" />
-      <mj-accordion-element icon-wrapped-url="https://i.imgur.com/Xvw0vjq.png" icon-unwrapped-url="https://i.imgur.com/KKHenWa.png" icon-height="24px" icon-width="24px" />
-      <mj-accordion-title font-family="Roboto, Open Sans, Helvetica, Arial, sans-serif" background-color="#fff" color="#031017" padding="15px" font-size="18px" />
-      <mj-accordion-text font-family="Open Sans, Helvetica, Arial, sans-serif" background-color="#fafafa" padding="15px" color="#505050" font-size="14px" />
-    </mj-attributes>
-  </mj-head>
-  <mj-body>
-    <mj-section padding="20px" background-color="#ffffff">
-      <mj-column background-color="#dededd">
-        <mj-accordion>
-          <mj-accordion-element>
-            <mj-accordion-title font-weight="bold" css-class="accordion-title">Why use an accordion?</mj-accordion-title>
-            <mj-accordion-text font-weight="bold">
-              <span style="line-height:20px">
-                Because emails with a lot of content are most of the time a very bad experience on mobile, mj-accordion comes handy when you want to deliver a lot of information in a concise way.
-              </span>
-            </mj-accordion-text>
-          </mj-accordion-element>
-          <mj-accordion-element>
-            <mj-accordion-title font-weight="700" css-class="accordion-title">How it works</mj-accordion-title>
-            <mj-accordion-text font-weight="700">
-              <span style="line-height:20px">
-                Content is stacked into tabs and users can expand them at will. If responsive styles are not supported (mostly on desktop clients), tabs are then expanded and your content is readable at once.
-              </span>
-            </mj-accordion-text>
-          </mj-accordion-element>
-        </mj-accordion>
-      </mj-column>
-    </mj-section>
-  </mj-body>
-</mjml>
-`
+describe('mj-accordion-title font-weight', function () {
+  it('should render correct font-weight in CSS style values on accordion-title', function () {
+    const input = `
+    <mjml>
+      <mj-head>
+        <mj-attributes>
+          <mj-accordion border="none" padding="1px" />
+          <mj-accordion-element icon-wrapped-url="https://i.imgur.com/Xvw0vjq.png" icon-unwrapped-url="https://i.imgur.com/KKHenWa.png" icon-height="24px" icon-width="24px" />
+          <mj-accordion-title font-family="Roboto, Open Sans, Helvetica, Arial, sans-serif" background-color="#fff" color="#031017" padding="15px" font-size="18px" />
+          <mj-accordion-text font-family="Open Sans, Helvetica, Arial, sans-serif" background-color="#fafafa" padding="15px" color="#505050" font-size="14px" />
+        </mj-attributes>
+      </mj-head>
+      <mj-body>
+        <mj-section padding="20px" background-color="#ffffff">
+          <mj-column background-color="#dededd">
+            <mj-accordion>
+              <mj-accordion-element>
+                <mj-accordion-title font-weight="bold" css-class="accordion-title">Why use an accordion?</mj-accordion-title>
+                <mj-accordion-text font-weight="bold">
+                  <span style="line-height:20px">
+                    Because emails with a lot of content are most of the time a very bad experience on mobile, mj-accordion comes handy when you want to deliver a lot of information in a concise way.
+                  </span>
+                </mj-accordion-text>
+              </mj-accordion-element>
+              <mj-accordion-element>
+                <mj-accordion-title font-weight="700" css-class="accordion-title">How it works</mj-accordion-title>
+                <mj-accordion-text font-weight="700">
+                  <span style="line-height:20px">
+                    Content is stacked into tabs and users can expand them at will. If responsive styles are not supported (mostly on desktop clients), tabs are then expanded and your content is readable at once.
+                  </span>
+                </mj-accordion-text>
+              </mj-accordion-element>
+            </mj-accordion>
+          </mj-column>
+        </mj-section>
+      </mj-body>
+    </mjml>
+    `
 
-const { html } = mjml(input)
+    const { html } = mjml(input)
 
-const $ = load(html)
+    const $ = load(html)
 
-// style values should be correct
-chai
-  .expect(
-    $('.accordion-title')
-      .map(function getAttr() {
-        const start = $(this).attr('style').indexOf('font-weight:') + 12
-        const end = $(this).attr('style').indexOf(';', start)
-        return $(this).attr('style').substring(start, end)
-      })
-      .get(),
-    'Font-weight in CSS style values on accordion-title',
-  )
-  .to.eql(['bold', '700'])
+    // style values should be correct
+    chai
+      .expect(
+        $('.accordion-title')
+          .map(function getAttr() {
+            const start = $(this).attr('style').indexOf('font-weight:') + 12
+            const end = $(this).attr('style').indexOf(';', start)
+            return $(this).attr('style').substring(start, end)
+          })
+          .get(),
+        'Font-weight in CSS style values on accordion-title',
+      )
+      .to.eql(['bold', '700'])
+  })
+})
--- packages/mjml/test/tableWidth.test.js
+++ packages/mjml/test/tableWidth.test.js
@@ -2,97 +2,87 @@ const chai = require('chai')
 const { load } = require('cheerio')
 const mjml = require('../lib')
 
-const input = `
-<mjml>
-    <mj-body>
+describe('mj-table width', function () {
+  it('should render correct width in CSS style values on mj-table', function () {
+    const input = `
+    <mjml>
+      <mj-body>
         <mj-wrapper>
-            <mj-section>
-                <mj-column>
-                    <mj-table css-class="table">
-                        <tr>
-                            <th style="border: 1px solid black;text-align: left;">
-                                Default Width
-                            </th>
-                            <td style="border: 1px solid black;">
-                                100%
-                            </td>
-                        </tr>
-                    </mj-table>
-                </mj-column>
-            </mj-section>
-            <mj-section>
-                <mj-column>
-                    <mj-table width="500px" css-class="table">
-                        <tr>
-                            <th style="border: 1px solid black;text-align: left;">
-                                Pixel Width
-                            </th>
-                            <td style="border: 1px solid black;">
-                                500px
-                            </td>
-                        </tr>
-                    </mj-table>
-                </mj-column>
-            </mj-section>
-            <mj-section>
-                <mj-column>
-                    <mj-table width="80%" css-class="table">
-                        <tr>
-                            <th style="border: 1px solid black;text-align: left;">
-                                Percentage Width
-                            </th>
-                            <td style="border: 1px solid black;">
-                                80%
-                            </td>
-                        </tr>
-                    </mj-table>
-                </mj-column>
-            </mj-section>
-            <mj-section css-class="section">
-                <mj-column>
-                    <mj-table width="auto" css-class="table">
-                        <tr>
-                            <th style="border: 1px solid black;text-align: left;">
-                                Auto Width
-                            </th>
-                            <td style="border: 1px solid black;">
-                                Auto
-                            </td>
-                        </tr>
-                    </mj-table>
-                </mj-column>
-            </mj-section>
+          <mj-section>
+            <mj-column>
+            <mj-table css-class="table">
+              <tr>
+                <th style="border: 1px solid black;text-align: left;">
+                  Default Width
+                </th>
+                <td style="border: 1px solid black;">100%</td>
+              </tr>
+            </mj-table>
+            </mj-column>
+          </mj-section>
+          <mj-section>
+            <mj-column>
+              <mj-table width="500px" css-class="table">
+                <tr>
+                  <th style="border: 1px solid black;text-align: left;">Pixel Width</th>
+                  <td style="border: 1px solid black;">500px</td>
+                </tr>
+              </mj-table>
+            </mj-column>
+          </mj-section>
+          <mj-section>
+            <mj-column>
+              <mj-table width="80%" css-class="table">
+                <tr>
+                  <th style="border: 1px solid black;text-align: left;">Percentage Width</th>
+                  <td style="border: 1px solid black;">80%</td>
+                </tr>
+              </mj-table>
+            </mj-column>
+          </mj-section>
+          <mj-section css-class="section">
+            <mj-column>
+              <mj-table width="auto" css-class="table">
+                <tr>
+                  <th style="border: 1px solid black;text-align: left;">Auto Width</th>
+                  <td style="border: 1px solid black;">Auto</td>
+                </tr>
+              </mj-table>
+            </mj-column>
+          </mj-section>
         </mj-wrapper>
-    </mj-body>
-</mjml>
-`
+      </mj-body>
+    </mjml>
+    `
 
-const { html } = mjml(input)
+    const { html } = mjml(input)
 
-const $ = load(html)
+    const $ = load(html)
 
-// width values should be correct
-chai
-  .expect(
-    $('.table table')
-      .map(function getAttr() {
-        return $(this).attr('width')
-      })
-      .get(),
-    'Width values on tables',
-  )
-  .to.eql(['100%', '500', '80%', 'auto'])
+    // width values should be correct
+    chai
+      .expect(
+        $('.table table')
+          .map(function getAttr() {
+            return $(this).attr('width')
+          })
+          .get(),
+        'Width values on tables',
+      )
+      .to.eql(['100%', '500', '80%', 'auto'])
 
-// style values should be correct
-chai
-  .expect(
-    $('.table table')
-      .map(function getAttr() {
-        const start = $(this).attr('style').indexOf('width:') + 6
-        const end = $(this).attr('style').indexOf(';', start)
-        return $(this).attr('style').substring(start, end)
-      })
-      .get(),
-    'Width in CSS style values on tables',
-  )
-  .to.eql(['100%', '500px', '80%', 'auto'])
+    // style values should be correct
+    chai
+      .expect(
+        $('.table table')
+          .map(function getAttr() {
+            const start = $(this).attr('style').indexOf('width:') + 6
+            const end = $(this).attr('style').indexOf(';', start)
+            return $(this).attr('style').substring(start, end)
+          })
+          .get(),
+        'Width in CSS style values on tables',
+      )
+      .to.eql(['100%', '500px', '80%', 'auto'])
+  })
+})
